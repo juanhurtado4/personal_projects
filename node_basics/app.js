a2cf3ba0328bc92c5d7590f2ec47d2e71085a70b
juan hurtado <juanhurtado4@outlook.com>
2017-12-22 01:39:00 -0500

    Fix: Implement error handling for parsing wrong data type

--- node_basics/app.js
+++ node_basics/app.js
@@ -1,26 +1,42 @@
 const https = require('https');
 
+function printError(error) {
+    console.log(error.message)
+}
+
 function printMessage(username, badgeCount, point) {
     const message = `${username} has ${badgeCount} total badge(s) and ${point} points in JavaScript`;
     console.log(message)
 }
 
 function getProfile(username) {
-    const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-        let body = "";
+    try {
+        const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+            let body = "";
 
-        response.on('data', data => {
-            body += data.toString();
+            response.on('data', data => {
+                body += data.toString();
+            });
+
+            response.on('end', () => {
+                try {
+                    const profile = JSON.parse(body);
+                    // console.log(typeof profile);
+                    // console.log(body);
+                    printMessage(username, profile.badges.length, profile.points.Javacript);
+                } catch (error) {
+                    printError(error)
+                }
+            });
+
+            
         });
-
-        response.on('end', () => {
-            const profile = JSON.parse(body);
-            // console.log(typeof profile);
-            // console.log(body);
-            printMessage(username, profile.badges.length, profile.points.Javacript);
+        request.on('error', error => { 
+            console.error(`Problem with request: ${error.message}`)
         });
-
-    });
+    } catch (error) {
+        printError(error)
+    }
 }
 
 const users = process.argv.slice(2);
