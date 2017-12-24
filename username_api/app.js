60587e17c7f8b3fc1431cefda6eac6bfbefbf50b
juan hurtado <juanhurtado4@outlook.com>
2017-12-23 23:53:12 -0500

    Feat: Create index route

--- username_api/app.js
+++ username_api/app.js
@@ -1,7 +1,16 @@
 var http = require('http');
+
 http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello');
-    }).listen(5000, '127.0.0.1');
+    homeRoute(request, response);
+}).listen(5000, '127.0.0.1');
 
 console.log('Server running at....')
+
+function homeRoute(request, response) {
+    if (request.url === '/') {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write('Header\n');
+        response.write('Search\n');
+        response.write('Footer\n');
+    }
+}
