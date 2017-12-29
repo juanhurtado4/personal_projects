79ab7e9da41b0a07d3633eb204757cfd4acf81e6
juan hurtado <juanhurtado4@outlook.com>
2017-12-28 23:54:42 -0500

    Feat: Implement ejs template

---
+++ express/ejs/app.js
@@ -0,0 +1,40 @@
+let express = require('express');
+let app = express();
+
+function removeUnderscore(string){
+    
+    return string.replace('_', ' ').toLowerCase()
+}
+
+function toTitleCase(string){
+    let newString = string.split(' ')
+    newString = newString.map(function(w) {
+        return w[0].toUpperCase() + w.slice(1).toLowerCase()
+    })
+
+    return newString.join(' ')
+}
+
+let test = 'juan hurtado'
+
+let title = toTitleCase(test)
+
+app.get('/', function(req, res){
+    res.render('home.ejs')
+});
+
+app.get('/favorite_book/:bookName', function(req, res){
+    let bookName = req.params.bookName;
+    bookName = removeUnderscore(bookName);
+    bookName = toTitleCase(bookName);
+
+    res.render('favorite_book.ejs', {'bookName':bookName});
+});
+
+
+app.listen('3000', () => {console.log('Listening on port 3000')});
+
+
+
+
+
