1e740def38e327b11ed55ada423bbeb94de9ae63
juan hurtado <juanhurtado4@outlook.com>
2017-12-29 19:50:31 -0500

    Feat: Add post request to append new todo into existing list of todos

--- express/express_post/app.js
+++ express/express_post/app.js
@@ -1,16 +1,20 @@
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
-    res.render('home');
+let todos = ['Buy screen cover', 'Finish JS project']
+
+app.post('/add_todos', function(req, res){
+    let newTodo = req.body.todo;
+    todos.push(newTodo)
+    res.redirect('/todos');
 })
 
-
-
-
-
-
+app.get('/todos', function(req, res){
+    res.render('todos', {todos: todos});
+})
 
 app.listen('3000', function() {console.log('Listening on port 3000')});
