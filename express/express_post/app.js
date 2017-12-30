c20d32089fac3893d382bb917c1969dc1263fb53
juan hurtado <juanhurtado4@outlook.com>
2017-12-29 19:04:35 -0500

    Chore: Create template for express server

---
+++ express/express_post/app.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    res.render('home');
+})
+
+
+
+
+
+
+
+app.listen('3000', function() {console.log('Listening on port 3000')});
--- username_api/router.js
+++ username_api/router.js
@@ -1,3 +1,5 @@
+var Profile = require("./profile.js");
+
 function home(request, response) {
     if (request.url === '/') {
         // response.setHeader("Content-Length", "");
@@ -10,10 +12,24 @@ function home(request, response) {
 
 function user(request, response) {
     var username = request.url.replace('/', '');
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('Header\n');
-    response.write(username + '\n');
-    response.end('Footer\n');
+    if (username.length > 0) {
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.write('Header\n');
+        var studentProfile = new Profile(username);
+        studentProfile.on('end', function(profileJSON) {
+
+            var values = {
+                avatarUrl: profileJSON.gravatar_url,
+                username:
+                badges:
+                javascriptPoints:
+            }
+        });
+        
+        studentProfile.on("error", console.error);
+        response.write(username + '\n');
+        response.end('Footer\n');
+    }
 
 }
 
