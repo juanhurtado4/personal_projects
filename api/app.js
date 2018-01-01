0cebd114c50ba5a6d68ce69b9be960b2df6c2272
juan hurtado <juanhurtado4@outlook.com>
2018-01-01 02:03:49 -0500

    Feat: Create results route to display json from omdb api

--- api/app.js
+++ api/app.js
@@ -1,14 +1,19 @@
 let request = require('request');
 let express = require('express');
 let app = express();
-
 app.set('view engine', 'ejs');
 
-app.get('/home', function(req, res){
-    'pass'
+app.get('/results', function(req, res){
+    
+    request('http://www.omdbapi.com/?apikey=88468f0b&s=california', 
+            function(error, response, body){
+                if (!error && response.statusCode == 200){
+                    res.send(body)
+                } else {
+                    console.error(error)
+                }
+    })
 })
 
-
-
-
+  
 app.listen('3000', () => console.log('Listening on port 3000'))
