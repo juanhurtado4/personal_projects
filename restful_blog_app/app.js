020cbeb15b7b6362e33077152a8b0321c943a4b4
juan hurtado <juanhurtado4@outlook.com>
2018-01-03 22:25:32 -0500

    Feat: Create blogs get route

--- restful_blog_app/app.js
+++ restful_blog_app/app.js
@@ -3,10 +3,36 @@ let mongoose = require('mongoose');
 let express = require('express');
 let app = express();
 
+// App Config
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 mongoose.connect('mongodb://localhost/restful_blog_app');
 app.use(bodyPser.urlencoded({extended: true}));
+
+// Mongoose Config
+let blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+})
+
+let Blog = mongoose.model('Blog', blogSchema);
+
+// Restful Routes
+app.get('/blogs', function(req, res){
+    Blog.find({}, function(err, blogs){
+        if (err) {
+            console.log('Error happeneded');
+        } else {
+            res.render('index', {blogs: blogs});
+        }
+    })
+})
+
+app.get('/', function(req, res){
+    res.redirect('/blogs')
+})
 
 app.listen('3000', () => {
     console.log('Server listening on port 3000')
